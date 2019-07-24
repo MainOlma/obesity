47366bd97345f2be8a3e812db48b72a0ef20ae1b
MainOlma <MainOlma@users.noreply.github.com>
2019-07-24 13:46:01 +0300

    Updates

---
+++ precache-manifest.2235feb50fbe7a7e06e2b8af062fd33e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685837126df13fbdbaae580ed57cada8",
+    "url": "/fat/index.html"
+  },
+  {
+    "revision": "e02d74c31071fd54213e",
+    "url": "/fat/static/css/main.edbfc7d3.chunk.css"
+  },
+  {
+    "revision": "c3a5ea74bfb0830f1084",
+    "url": "/fat/static/js/2.8733c675.chunk.js"
+  },
+  {
+    "revision": "e02d74c31071fd54213e",
+    "url": "/fat/static/js/main.f8af26e2.chunk.js"
+  },
+  {
+    "revision": "b1c8d20d905078d4288d",
+    "url": "/fat/static/js/runtime~main.e46317c3.js"
+  },
+  {
+    "revision": "366603145cbbc33a7be34cf7effbdf93",
+    "url": "/fat/static/media/LeagueMonoVariable.36660314.ttf"
+  },
+  {
+    "revision": "a29f1d060890046bb36b9832630fd87e",
+    "url": "/fat/static/media/fat.a29f1d06.csv"
+  },
+  {
+    "revision": "4405af313862e1d0edcc65b12eb41e9b",
+    "url": "/fat/static/media/gridMap.4405af31.csv"
+  }
+]);
